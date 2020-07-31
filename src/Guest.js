efbc996520259f7c9cee41b19ce748ccf05da508
Naomi Ware <nware1066@yahoo.com>
2020-07-31 11:38:42 -0600

    Add properties to Guest class to match rubric expectations

--- src/Guest.js
+++ src/Guest.js
@@ -2,6 +2,9 @@ class Guest {
   constructor() {
     this.id = guest.id;
     this.name = guest.name;
+    this.password = 'overlook2020'
+    this.bookings = [];
+    this.totalSpent = 0;
   }
 }
-export default ;
+export default Guest;
