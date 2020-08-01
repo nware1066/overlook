cb99bfce1d653ee23f4f3276df1a57920e52c15f
Naomi Ware <nware1066@yahoo.com>
2020-08-01 11:17:58 -0600

    Add imports to Manager test file

--- test/manager.js
+++ test/manager.js
@@ -2,4 +2,6 @@ import chai from 'chai';
 const expect = chai.expect;
 import spies from 'chai-spies';
 chai.use(spies);
-import Manager from '../src/Manager'
+import Manager from '../src/Manager';
+import Guest from '../src/Guest';
+import { guests, rooms, bookings } from './test/testData'
